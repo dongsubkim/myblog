47aac4869373c8917cc04729bf8c426f1d4ee432
DongSubKim <dskim9218@gmail.com>
2021-04-28 21:52:03 +0900

    login, register page, authentication logic added

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ const ExpressError = require('./utils/expressError');
 const session = require('express-session');
 const flash = require('connect-flash');
 const postRoutes = require('./routes/posts');
+const userRoutes = require('./routes/users');
 const commentRoutes = require('./routes/comments');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/myblog';
 
@@ -32,6 +37,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize());
 
 const sessionConfig = {
     name: 'session',
@@ -48,13 +54,21 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
-    // res.locals.currentUser = req.user;
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 })
 
+app.use('/', userRoutes);
 app.use('/posts', postRoutes);
 app.use('/posts/:id/comments', commentRoutes);
 
